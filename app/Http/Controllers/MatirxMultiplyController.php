003b4a0e1f1412e0da1a702890c6fa507447171f
Olalekan Agbaje <olalekanagbaje@gmail.com>
2021-05-01 17:06:40 +0100

    implemented test testArrayMultiplicationIsValid

--- app/Http/Controllers/MatirxMultiplyController.php
+++ app/Http/Controllers/MatirxMultiplyController.php
@@ -12,9 +12,13 @@ class MatirxMultiplyController extends Controller
     public int $arrayCount;
     public array $array1;
     public array $array2;
+    public int $array1ColCount;
+    public int $array1RowCount;
+    public int $array2ColCount;
+    public int $array2RowCount;
 
     public function index()
-    {        
+    {
         $rawArray = request()->all();
         
         $this->arrayCount = $this->countArray($rawArray);
@@ -25,18 +29,43 @@ class MatirxMultiplyController extends Controller
         $this->array2 = $rawArray[1];
 
         $this->Array1ColumnsEqualArray2Rows();
-        
 
-        dd($this->arrayCount);
+        return ($this->multiplyArrays());
         
     }
 
-    private function countArray(array $rawArray)
+    private function multiplyArrays(): array
+    {
+        /* we perfom the array multiplication here */
+        $r = $this->array1RowCount;
+        $c = $this->array2ColCount;
+        $r2 = $this->array2RowCount;
+        
+        $output = [];
+        for ($i = 0; $i < $r; $i++) {
+
+            for ($j = 0; $j < $c; $j++) { 
+
+                $output[$i][$j] = 0;
+
+                for ($k = 0; $k < $r2; $k++) {
+                    $output[$i][$j] += $this->array1[$i][$k] * $this->array2[$k][$j];
+                }
+
+            }
+
+        }
+
+        return $output;
+        
+    }
+
+    private function countArray(array $rawArray): int
     {
         return count($rawArray);
     }
 
-    private function arrayCountIsValid()
+    private function arrayCountIsValid() : bool
     {
         if($this->arrayCount != 2)
         {
@@ -46,12 +75,17 @@ class MatirxMultiplyController extends Controller
         return true;
     }
 
-    private function Array1ColumnsEqualArray2Rows()
+    private function Array1ColumnsEqualArray2Rows(): bool
     {
         if(count($this->array1[0]) != count($this->array2))
         {
             throw new Exception("Number of columns in the 1st Matirx Must be equal to number of rows in the 2nd Matrix.", 500);             
         }
+
+        $this->array1ColCount = count($this->array1[0]);
+        $this->array1RowCount = count($this->array1);
+        $this->array2ColCount = count($this->array2[0]);
+        $this->array2RowCount = count($this->array2);
 
         return true;
     }
--- tests/Feature/MatrixValidationTest.php
+++ tests/Feature/MatrixValidationTest.php
@@ -8,7 +8,7 @@ use Tests\TestCase;
 
 class MatrixValidationTest extends TestCase
 {
-    
+
     public function testRequestContaintsTwoArrays()
     {
         $response = $this->post('/api', $this->am([1]));
@@ -18,34 +18,55 @@ class MatrixValidationTest extends TestCase
 
     public function testArray1ColumnsEqualArray2Rows()
     {
-        // $this->withoutExceptionHandling();
-        $response = $this->post('/api', $this->data());
+        $badArray = [
+            1 => [
+                [2, 9, 0],
+                [2, 9, 0],
+            ]
+        ];
+        $response = $this->post('/api', $this->am($badArray));
 
         $response->assertStatus(500);
     }
 
-    private function am(array $newData)
+    public function testArrayMultiplicationIsValid()
     {
-        return array_merge($this->data(),$newData);
+        $this->withoutExceptionHandling();
+        $response = $this->post('/api', $this->data());
+        $result = $response->getData();
+
+        $this->assertEquals($result, $this->correctResponse());
     }
 
-    public function data()
+    private function am(array $newData)
+    {
+        return array_merge($this->data(), $newData);
+    }
+
+    private function correctResponse()
+    {
+        return [
+            [50, 42, 42], 
+            [25, 96, 26]
+        ];
+    }
+
+    private function data()
     {
         $matrixA = [
-            [3,2,1,5],
-            [9,1,3,0],
+            [3, 2, 1, 5],
+            [9, 1, 3, 0],
         ];
         $matrixB = [
-            [2,9,0],
-            [2,9,0],
-            [1,3,5],
-            [2,4,7],
-            [8,1,5],
+            [2, 9, 0],
+            [1, 3, 5],
+            [2, 4, 7],
+            [8, 1, 5],
         ];
 
         $data = [
             $matrixA, $matrixB
-        ]; 
+        ];
 
         return $data;
     }
