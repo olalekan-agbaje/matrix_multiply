1b25ba591b4f60bbc4cd8db94b7d7684ab2948cc
Olalekan Agbaje <olalekanagbaje@gmail.com>
2021-05-01 14:16:31 +0100

    set default message on a GET call to the API

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/',function(){
+    return 'This is an API to multiply two matrices. Developed by Olalekan Agbaje - olalekanagbaje@gmail.com';
+});
