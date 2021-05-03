26e120ba7ca36528cac8824417bdff318bb0ee62
Olalekan Agbaje <olalekanagbaje@gmail.com>
2021-05-03 04:14:53 +0100

    moved arrayNumToAlpha to reside in NumberToAlphabets trait

--- app/Http/Controllers/Traits/NumberToAlphabetsTrait.php
+++ app/Http/Controllers/Traits/NumberToAlphabetsTrait.php
@@ -19,4 +19,20 @@ trait NumberToAlphabetsTrait
     return $i > 0 ? $this->NumToAlpha($i) . $letters[$num % 26] : $letters[$num % 26];
 
   }
+  
+  private function arrayNumToAlpha(array $matrix): array
+  {
+      $row = count($matrix);
+      $col = count($matrix[0]);
+
+      for ($i = 0; $i < $row; $i++) {
+
+          for ($j = 0; $j < $col; $j++) {
+
+              $matrix[$i][$j] = $this->numtoAlpha($matrix[$i][$j]);
+          }
+      }
+
+      return $matrix;
+  }
 }
